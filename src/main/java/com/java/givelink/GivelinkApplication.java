6d84058c58b86c97c870677309492b5adecffb59
heedayday <heeday3001@gmail.com>
2025-03-12 01:02:19 +0900

    front

---
+++ src/main/java/com/java/givelink/GivelinkApplication.java
@@ -0,0 +1,13 @@
+package com.java.givelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivelinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/givelink/GivelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.givelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
